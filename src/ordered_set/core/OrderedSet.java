02553d275d3eccfb807fba92f932303aa5e04cf7
Alan Malloy <alan@malloys.org>
2012-04-20 15:25:26 -0700

    Put braces around all block structures

--- src/ordered_set/core/OrderedSet.java
+++ src/ordered_set/core/OrderedSet.java
@@ -55,21 +55,24 @@ public class OrderedSet extends AFn implements IObj, IEditableCollection, IPersi
 
   @Override
   public IPersistentSet disjoin(Object item) throws Exception {
-    if (!contains(item))
+    if (!contains(item)) {
       return this;
+    }
 
     ITransientVector new_order = PersistentVector.EMPTY.asTransient();
     for (ISeq s = seq(); s != null; s = s.next()) {
-      if (!Util.equiv(item, s.first()))
+      if (!Util.equiv(item, s.first())) {
         new_order = (ITransientVector)new_order.conj(s.first());
+      }
     }
     return new OrderedSet(_meta, items.disjoin(item), new_order.persistent());
   }
 
   @Override
   public IPersistentSet cons(Object item) {
-    if (contains(item))
+    if (contains(item)) {
       return this;
+    }
     return new OrderedSet(_meta, (IPersistentSet)items.cons(item), order.cons(item));
   }
 
@@ -106,8 +109,9 @@ public class OrderedSet extends AFn implements IObj, IEditableCollection, IPersi
   @Override
   public boolean containsAll(Collection c) {
     for (Object item : c) {
-      if (!contains(item))
+      if (!contains(item)) {
         return false;
+      }
     }
     return true;
   }
@@ -144,12 +148,14 @@ public class OrderedSet extends AFn implements IObj, IEditableCollection, IPersi
 
   @Override
   public boolean equals(Object obj) {
-    if (!(obj instanceof Set))
+    if (!(obj instanceof Set)) {
       return false;
+    }
     Set s = (Set)obj;
 
-    if (s.size() != count() || s.hashCode() != hashCode())
+    if (s.size() != count() || s.hashCode() != hashCode()) {
       return false;
+    }
     return containsAll(s);
   }
 
@@ -178,15 +184,17 @@ public class OrderedSet extends AFn implements IObj, IEditableCollection, IPersi
 
   @Override
   public Object[] toArray(Object[] a) {
-    if (count() > a.length)
+    if (count() > a.length) {
       return toArray();
+    }
 
     ISeq s = seq();
     for (int i = 0; s != null; ++i, s = s.next()) {
       a[i] = s.first();
     }
-    if (a.length > count())
+    if (a.length > count()) {
       a[count()] = null;
+    }
     return a;
   }
 
@@ -241,16 +249,19 @@ public class OrderedSet extends AFn implements IObj, IEditableCollection, IPersi
 
     @Override
     public ITransientSet conj(Object obj) {
-      if (contains(obj))
+      if (contains(obj)) {
         return this;
+      }
 
       ITransientSet s = (ITransientSet)items.conj(obj);
-      if (s != items)
+      if (s != items) {
         items = s;
+      }
 
       ITransientVector v = (ITransientVector)order.conj(obj);
-      if (v != order)
+      if (v != order) {
         order = v;
+      }
 
       return this;
     }
@@ -269,19 +280,22 @@ public class OrderedSet extends AFn implements IObj, IEditableCollection, IPersi
 
     @Override
     public ITransientSet disjoin(Object obj) throws Exception {
-      if (!contains(obj))
+      if (!contains(obj)) {
         return this;
+      }
 
       ITransientSet set = items.disjoin(obj);
-      if (set != items)
+      if (set != items) {
         items = set;
+      }
 
       ITransientVector new_order = PersistentVector.EMPTY.asTransient();
       int max = order.count();
       for (int i = 0; i < max; i++) {
         Object item = order.valAt(i);
-        if (!Util.equiv(item, obj))
+        if (!Util.equiv(item, obj)) {
           new_order = (ITransientVector)new_order.conj(item);
+        }
       }
       order = new_order;
 
