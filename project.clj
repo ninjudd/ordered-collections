49fe996e8a0a58d610f93fa390c2132911b5fbea
Alan Malloy <alan@malloys.org>
2012-04-20 18:01:49 -0700

    lein2-friendly project.clj

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject ordered-set "0.2.3"
   :description "A Clojure set that maintains the insertion order of items"
-  :dependencies [[clojure "1.2.0"]])
+  :dependencies [[org.clojure/clojure "1.2.0"]]
+  :java-source-paths ["src"])
