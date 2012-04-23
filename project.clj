eba05a90fe05f87a5a779b650864c9eb1e69a289
Alan Malloy <alan@malloys.org>
2012-04-23 13:30:16 -0700

    0.3.0 - add OrderedMap

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ordered-set "0.2.3"
+(defproject ordered-set "0.3.0"
   :description "A Clojure set that maintains the insertion order of items"
   :dependencies [[org.clojure/clojure "1.2.0"]]
   :java-source-paths ["src"])
