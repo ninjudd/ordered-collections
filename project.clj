07356290b5495e8bdb992198db2e2547be6df440
Justin Balthrop <git@justinbalthrop.com>
2012-05-01 18:26:04 -0700

    version 0.4.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ordered-collections "0.4.1"
+(defproject ordered-collections "0.4.2"
   :description "A Clojure set and map that maintain the insertion order of items, implemented in Java."
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :java-source-paths ["src"])
